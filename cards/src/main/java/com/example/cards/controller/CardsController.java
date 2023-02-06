ff47aebecf4b90cc3a51fb04af86dd6544a298db
Patrik Miculka <MiPatrik@gmail.com>
2023-02-06 20:44:16 +0100

    init commit

---
+++ cards/src/main/java/com/example/cards/controller/CardsController.java
@@ -0,0 +1,24 @@
+package com.example.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cards.model.Cards;
+import com.example.cards.model.Customer;
+import com.example.cards.repository.CardsRepository;
+
+@RestController
+public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+        return cardsRepository.findByCustomerId(customer.getCustomerId());
+    }
+}
