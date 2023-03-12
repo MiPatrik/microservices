8525abdcecfeb55bc634efffc19d5cdf261aabb1
patrik <mipatrik@gmail.com>
2023-03-12 13:22:56 +0100

    properties refreshing

--- loans/src/main/java/com/example/loans/LoansApplication.java
+++ loans/src/main/java/com/example/loans/LoansApplication.java
@@ -3,11 +3,13 @@ package com.example.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.example.loans.controller") })
 @EnableJpaRepositories("com.example.loans.repository")
 @EntityScan("com.example.loans.model")
